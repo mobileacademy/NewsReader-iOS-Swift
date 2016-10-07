f66f61b1e7da76b65fd9530aac7d39f8147fdb05
Radu Dita <hades2510@yahoo.com>
2016-10-07 12:32:51 +0300

    added class for Story cell

---
+++ NewsReaderSwift/StoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  StoryCell.swift
+//  NewsReaderSwift
+//
+//  Created by Mac on 07/10/16.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StoryCell: UITableViewCell{
+    
+}
