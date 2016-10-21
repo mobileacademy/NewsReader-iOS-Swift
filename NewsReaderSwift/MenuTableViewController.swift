8f14a9b6e7ff5d3984f8526f8d5ee55cd7e25d62
Radu Dita <hades2510@yahoo.com>
2016-10-21 13:31:35 +0300

    added class for uitableviewcontroller

---
+++ NewsReaderSwift/MenuTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuTableViewController.swift
+//  NewsReaderSwift
+//
+//  Created by Mac on 21/10/2016.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuTableViewController: UITableViewController{
+    
+}
