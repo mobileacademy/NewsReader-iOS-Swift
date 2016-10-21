2601d80338e7d95905e34b0284f68c22afd54030
Radu Dita <hades2510@yahoo.com>
2016-10-21 13:52:34 +0300

    added enum for story type

---
+++ NewsReaderSwift/StoryType.swift
@@ -0,0 +1,15 @@
+//
+//  StoryType.swift
+//  NewsReaderSwift
+//
+//  Created by Mac on 21/10/2016.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+
+
+enum StoyType{
+    case Top
+    case Latest
+}
