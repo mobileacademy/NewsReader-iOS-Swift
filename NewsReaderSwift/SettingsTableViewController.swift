3e44d1aea65e1cde4f3efdbcc61bc875a21b251f
Radu Dita <hades2510@yahoo.com>
2016-10-21 16:33:35 +0300

    added switch for bg

---
+++ NewsReaderSwift/SettingsTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTableViewController.swift
+//  NewsReaderSwift
+//
+//  Created by Mac on 21/10/2016.
+//  Copyright © 2016 mobileacademy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController{
+    
+}
